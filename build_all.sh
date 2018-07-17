0916dd098ca572302d977c182fe708801a827a5f
ShamsUlAzeem <shamsazeem20@gmail.com>
2018-07-17 19:28:19 +0500

    Reverted the build_all.sh script

--- build_all.sh
+++ build_all.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
 # java
-java -jar E:/Projects/Git/swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i swagger.yaml -l java -o java -c config/java.json
+swagger-codegen generate -i swagger.yaml -l java -o java -c config/java.json
 
 # python
-java -jar E:/Projects/Git/swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i swagger.yaml -l python -o python -c config/python.json
+swagger-codegen generate -i swagger.yaml -l python -o python -c config/python.json
 
 # javascript
-java -jar E:/Projects/Git/swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i swagger.yaml -l javascript -o javascript -c config/javascript.json
+swagger-codegen generate -i swagger.yaml -l javascript -o javascript -c config/javascript.json
 
 # ruby
-java -jar E:/Projects/Git/swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i swagger.yaml -l ruby -o ruby -c config/ruby.json
+swagger-codegen generate -i swagger.yaml -l ruby -o ruby -c config/ruby.json
 
 # go
-java -jar E:/Projects/Git/swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i swagger.yaml -l go -o go -c config/go.json
+swagger-codegen generate -i swagger.yaml -l go -o go -c config/go.json
 
 # scala
-java -jar E:/Projects/Git/swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i swagger.yaml -l scala -o scala -c config/scala.json
+swagger-codegen generate -i swagger.yaml -l scala -o scala -c config/scala.json
 
 # csharp
-java -jar E:/Projects/Git/swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i swagger.yaml -l csharp -o csharp -c config/csharp.json
+swagger-codegen generate -i swagger.yaml -l csharp -o csharp -c config/csharp.json
 
 # rust
-java -jar E:/Projects/Git/swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i swagger.yaml -l rust -o rust -c config/rust.json
+swagger-codegen generate -i swagger.yaml -l rust -o rust -c config/rust.json
 
 # android
-java -jar E:/Projects/Git/swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i swagger.yaml -l android -o android -c config/android.json
+swagger-codegen generate -i swagger.yaml -l android -o android -c config/android.json
 
 # flash
-java -jar E:/Projects/Git/swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i swagger.yaml -l flash -o flash -c config/flash.json
+swagger-codegen generate -i swagger.yaml -l flash -o flash -c config/flash.json
 
 # swift3
-java -jar E:/Projects/Git/swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i swagger.yaml -l swift3 -o swift3 -c config/swift3.json
+swagger-codegen generate -i swagger.yaml -l swift3 -o swift3 -c config/swift3.json
