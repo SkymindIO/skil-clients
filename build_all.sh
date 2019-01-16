b9811fbb1f91e02fe5593cc125be5101bc947819
ShamsUlAzeem <shamsazeem20@gmail.com>
2019-01-16 15:06:43 +0500

    Updated configs, build scripts and open api specs

--- build_all.sh
+++ build_all.sh
@@ -5,41 +5,41 @@
 # ---------------------------------------------------------
 
 # java
-java -jar swagger-codegen-cli.jar generate -Dapis=Default -Dmodels -DsupportingFiles -i swagger.yaml -l java -o java -c config/java.json
+java -jar openapi-generator-cli.jar generate -Dapis=Default -Dmodels -DsupportingFiles -i openapi.yaml -g java -o java -c config/java.json
 
 # python
-java -jar swagger-codegen-cli.jar  generate -Dapis=Default -Dmodels -DsupportingFiles -i swagger.yaml -l python -o python -c config/python.json
+java -jar openapi-generator-cli.jar  generate -Dapis=Default -Dmodels -DsupportingFiles -i openapi.yaml -g python -o python -c config/python.json
 
 # javascript
-java -jar swagger-codegen-cli.jar generate -Dapis=Default -Dmodels -DsupportingFiles -i swagger.yaml -l javascript -o javascript -c config/javascript.json
+java -jar openapi-generator-cli.jar generate -Dapis=Default -Dmodels -DsupportingFiles -i openapi.yaml -g javascript -o javascript -c config/javascript.json
 
 # ruby
-java -jar swagger-codegen-cli.jar generate -Dapis=Default -Dmodels -DsupportingFiles -i swagger.yaml -l ruby -o ruby -c config/ruby.json
+java -jar openapi-generator-cli.jar generate -Dapis=Default -Dmodels -DsupportingFiles -i openapi.yaml -g ruby -o ruby -c config/ruby.json
 
 # go
-java -jar swagger-codegen-cli.jar generate -Dapis=Default -Dmodels -DsupportingFiles -i swagger.yaml -l go -o go -c config/go.json
+java -jar openapi-generator-cli.jar generate -Dapis=Default -Dmodels -DsupportingFiles -i openapi.yaml -g go -o go -c config/go.json
 
 # scala
-java -jar swagger-codegen-cli.jar generate -Dapis=Default -Dmodels -DsupportingFiles -i swagger.yaml -l scala -o scala -c config/scala.json
+java -jar openapi-generator-cli.jar generate -Dapis=Default -Dmodels -DsupportingFiles -i openapi.yaml -g scala-httpclient -o scala-httpclient -c config/scala-httpclient.json
 
 # csharp
-java -jar swagger-codegen-cli.jar generate -Dapis=Default -Dmodels -DsupportingFiles -i swagger.yaml -l csharp -o csharp -c config/csharp.json
+java -jar openapi-generator-cli.jar generate -Dapis=Default -Dmodels -DsupportingFiles -i openapi.yaml -g csharp -o csharp -c config/csharp.json
 
 # rust
-java -jar swagger-codegen-cli.jar generate -Dapis=Default -Dmodels -DsupportingFiles -i swagger.yaml -l rust -o rust -c config/rust.json
+java -jar openapi-generator-cli.jar generate -Dapis=Default -Dmodels -DsupportingFiles -i openapi.yaml -g rust -o rust -c config/rust.json
 
 # android
-java -jar swagger-codegen-cli.jar generate -Dapis=Default -Dmodels -DsupportingFiles -i swagger.yaml -l android -o android -c config/android.json
+java -jar openapi-generator-cli.jar generate -Dapis=Default -Dmodels -DsupportingFiles -i openapi.yaml -g android -o android -c config/android.json
 
 # flash
-java -jar swagger-codegen-cli.jar generate -Dapis=Default -Dmodels -DsupportingFiles -i swagger.yaml -l flash -o flash -c config/flash.json
+java -jar openapi-generator-cli.jar generate -Dapis=Default -Dmodels -DsupportingFiles -i openapi.yaml -g flash -o flash -c config/flash.json
 
 # swift3
-java -jar swagger-codegen-cli.jar generate -Dapis=Default -Dmodels -DsupportingFiles -i swagger.yaml -l swift3 -o swift3 -c config/swift3.json
+java -jar openapi-generator-cli.jar generate -Dapis=Default -Dmodels -DsupportingFiles -i openapi.yaml -g swift4 -o swift4 -c config/swift4.json
 
 # html (For documentation site)
-java -jar swagger-codegen-cli.jar generate -Dapis=Default -Dmodels -DsupportingFiles -i swagger.yaml -l html -o html
+java -jar openapi-generator-cli.jar generate -Dapis=Default -Dmodels -DsupportingFiles -i openapi.yaml -g html -o html
 
 # html2 (For documentation site)
-java -jar swagger-codegen-cli.jar generate -Dapis=Default -Dmodels -DsupportingFiles -i swagger.yaml -l html2 -o html2
+java -jar openapi-generator-cli.jar generate -Dapis=Default -Dmodels -DsupportingFiles -i openapi.yaml -g html2 -o html2
 # ---------------------------------------------------------
